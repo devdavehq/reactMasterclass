1d5a4a278e6447646c988b323f06859632bf1351
unknown <your_email@example.com>
2025-01-09 15:33:59 +0100

    initialized new project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+          <div className='bg-emerald-600 text-xl text-sky-300 text-center'>
+            hello world
+          </div>
+  )
+}
+
+export default App
